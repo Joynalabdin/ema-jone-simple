49f80fac776c7065412a15004a6317d3953c9b21
unknown <Joynalabdin>
2020-03-16 18:01:44 +0600

    add some data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,23 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
 const Shop = () => {
+         const first10 = fakeData.slice(0,10);
+         const [products, setProducts] = useState(first10);   
     return (
-        <div>
-          <h1> This is shop </h1>  
+        <div className="shop-container">
+          <div className="product-container">
+         
+          <ul>
+            {
+              products.map(products=><li>{products.name}</li>)
+            }
+          </ul>
+          </div>
+          <div className="cart-container">
+          <h1> This is cart container </h1>  
+          </div>
+          
         </div>
     );
 };
