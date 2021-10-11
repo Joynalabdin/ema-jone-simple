121a97354f3b2134edf381384ad1b68990239204
Md Joynal Abdin Robel <robel.joynal@gmail.com>
2021-10-11 12:05:32 +0600

    fixed product price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Cart = (props) => {
     
     const cart = props.cart
-    // const total = cart.reduce( (total, prd) => total + prd.price, 0);
-    let total = 0;
-    for(let i = 0; i<cart.length; i++ ){
-        const product = cart[i];
-        total = total + product.price * product.quantity;
-    }
+    const total = cart.reduce( (total, prd) => total + prd.price, 0);
+    // let total = 0;
+    // for(let i = 0; i<cart.length; i++ ){
+    //     const product = cart[i];
+    //     total = total + product.price * product.quantity;
+    // }
 
     let shipping =0;
     if(total > 15 ){
@@ -25,11 +25,12 @@ const Cart = (props) => {
     // const tax =Math.round (total/10);
     const tax = (total / 10).toFixed(2);
     const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items Order:{cart.length} </h5>
-            <p><small>Product Price :{total} </small></p>
+            <p><small>Product Price :{total.toFixed(2)} </small></p>
             <p><small>Shipping cost :{shipping} </small></p>
             <p><small>Text+Vat :{tax} </small></p>
             <p><small>Total Price:{grandTotal} </small></p>
